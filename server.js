bd53f54cd3dc25de058e769bb5feb86789a58878
John Shewchuk <johnshewchuk@MacBook-Air.local>
2015-06-29 16:23:05 -0700

    Node

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require("express");
+
+var app = express();
+
+app.get("/", function (req, res) {
+	res.send("Hello World JS");
+}
+
+var port = process.env.port || 3000;
+app.listen(port);
