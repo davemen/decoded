aaa9117c9cb618afcd1d7fbba0768a5f3e21deb9
John Shewchuk <johnshewchuk@MacBook-Air.local>
2015-06-29 17:15:15 -0700

    Updated server

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ var express = require("express");
 
 var app = express();
 
+app.use(express.static('.'));
+
 app.get("/", function (req, res) {
 	res.send("Hello World JS");
 });
+
 
 var port = process.env.port || 3000;
 app.listen(port);
